8644f52ed9cbd5e476db5db6ae5d879ce1778571
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-14 19:16:10 -0300

    navbar-setup

---
+++ src/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+import './component.css';
+
+const NavBar = () => (
+<nav><ul><li><NavLink>Home</NavLink></li></ul></nav>
+);
+
+export default NavBar;
