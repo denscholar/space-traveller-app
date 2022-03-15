03f6b786d879ec9b5c3d1f19a6a4348961618208
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-15 16:30:39 -0300

    setup

--- src/Redux/Reducers/index.js
+++ src/Redux/Reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import rocketReducer from './RocketReducer';
+import missionsReducer from './MissionReducer';
 
 const reducers = combineReducers({
   rocket: rocketReducer,
+  missionsReducer,
+ 
 });
 
 export default reducers;
