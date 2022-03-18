7ad7ae68d79b3f1bb951c2d0764e1f59030637c3
Mauro Vasconcelos <88060989+vanillainanutshell@users.noreply.github.com>
2022-03-18 17:37:49 -0300

    mission testing

---
+++ src/components/tests/missionReducer.test.js
@@ -0,0 +1,32 @@
+import missionReducer, { missionReserve } from '../../redux/Missions/MissionReducer';
+
+describe('Inital state of the reducer', () => {
+  test('test intial state: ', () => {
+    expect(missionReducer(undefined, {})).toEqual({
+      missions: [],
+    });
+  });
+});
+
+describe('Reserve mission test', () => {
+  test('should return missions.reserved as false: ', () => {
+    const exampleInitialState = {
+      missions: [
+        {
+          id: '9D1B7E0',
+          reserved: true,
+        },
+      ],
+    };
+    expect(missionReducer(exampleInitialState, missionReserve('9D1B7E0'))).toEqual(
+      {
+        missions: [
+          {
+            id: '9D1B7E0',
+            reserved: false,
+          },
+        ],
+      },
+    );
+  });
+});
